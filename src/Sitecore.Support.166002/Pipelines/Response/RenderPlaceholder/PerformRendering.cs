654692e0ec0d02b1bced85f9d6d98f76d47d78dc
DK\msb <msb@sitecore.net>
2017-06-02 14:12:23 -0400

    Verification that rendering renderer is not null before execution is perfromed

---
+++ src/Sitecore.Support.166002/Pipelines/Response/RenderPlaceholder/PerformRendering.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Support.Mvc.Pipelines.Response.RenderPlaceholder
+{
+    using Sitecore.Mvc.Pipelines.Response.RenderPlaceholder;
+    using Sitecore.Mvc.Presentation;
+    using System.IO;
+
+    public class PerformRendering : Sitecore.Mvc.Pipelines.Response.RenderPlaceholder.PerformRendering
+    {
+        protected override void Render(string placeholderName, TextWriter writer, RenderPlaceholderArgs args)
+        {
+            foreach (Rendering rendering in this.GetRenderings(placeholderName, args))
+            {
+                if (rendering != null && rendering.Renderer != null)
+                {
+                    using (this.CreateCyclePreventer(placeholderName, rendering))
+                    {
+                        this.ProcessRenderRendering(rendering, writer);
+                    }
+                }
+            }
+        }
+    }
+}
